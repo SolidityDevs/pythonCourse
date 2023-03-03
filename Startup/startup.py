cdb575c245b19726a3ad8f4bd8a0384e90972250
Cointendprice <89971340+Cointendprice@users.noreply.github.com>
2023-03-03 23:15:57 +0100

    start up

---
+++ Startup/startup.py
@@ -0,0 +1,14 @@
+hello this is python
+
+
+
+
+hello 
+welcome
+to
+python
+development
+
+
+
+print('hello')
---
+++ start.py
@@ -0,0 +1,10 @@
+#print('hello world')
+print('we made a single line comment')
+"""hello
+welcome
+to
+our 
+class
+"""
+
+print('okay we made a multi line comment')
