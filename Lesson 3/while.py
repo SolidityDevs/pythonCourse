e90141993eae73e356e2d244da4494e499f4a610
Cointendprice <89971340+Cointendprice@users.noreply.github.com>
2023-03-10 21:30:02 +0100

    plopl

---
+++ Lesson 3/while.py
@@ -0,0 +1,13 @@
+numbers = 1
+
+while numbers < 10:
+    print(numbers)
+    
+    #if numbers == 5:
+        #break
+        
+    #if numbers == 5:
+        #continue
+    numbers  += 1
+else:
+    print(f'numbers is no longer less than 10\ncurrent number: {numbers}')
